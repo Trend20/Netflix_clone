75d99ce94a838172627ccf471996cbfe000a13a3
Trend20 <enockomondi305@gmail.com>
2021-04-26 22:18:42 +0300

    navbar commit

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.designmantic.com%2Fblog%2Fwp-content%2Fuploads%2F2016%2F07%2FNetflix-Logo.png&imgrefurl=https%3A%2F%2Fwww.designmantic.com%2Fblog%2Fnetflix-revamps-its-logo%2F&tbnid=nC3o5x7fqXTCYM&vet=12ahUKEwin8d3MzpzwAhX4DGMBHXk5CuoQMygCegUIARDYAQ..i&docid=VCcL0M6enYsN4M&w=750&h=512&q=Netflix%20logo&ved=2ahUKEwin8d3MzpzwAhX4DGMBHXk5CuoQMygCegUIARDYAQ"
+             className="nav_logo" alt="Netflix Logo"/>
+        </div>
+    )
+}
+
+export default Nav
